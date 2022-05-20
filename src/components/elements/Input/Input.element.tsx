a1cba863042c9171614ec659cd909b84c9e0ae99
Nikita Lafinskiy <niklafinskiy@gmail.com>
2022-05-20 15:58:27 +0300

    mobile version tweaks

--- src/components/elements/Input/Input.element.tsx
+++ src/components/elements/Input/Input.element.tsx
@@ -3,11 +3,11 @@ import { InputProps } from "types/components/elements";
 import { ElementHelpers } from "utils/elements/ElementsHelpers";
 import "./Input.scss";
 
-function Input({ name, style, className, type, ...rest }: InputProps) {
+function Input({ name, style, className, ...rest }: InputProps) {
   const classes = ElementHelpers.convertClass("elements__input", className);
   return (
     <div className={classes} style={style}>
-      <Field autocomplete='off' as={type} name={name} {...rest} />
+      <Field autocomplete='off' name={name} {...rest} />
       <ErrorMessage name={name} />
     </div>
   );
